54f836d8424374d7008d72d536e854763226d9b5
refanzzzz <refandasuryasaputra@gmail.com>
2022-03-29 17:18:41 +0700

    Membuat Tampilan Login

--- app/src/main/java/com/refanzzzz/aplikasipendataan/MainActivity.java
+++ app/src/main/java/com/refanzzzz/aplikasipendataan/MainActivity.java
@@ -3,12 +3,43 @@ package com.refanzzzz.aplikasipendataan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView username, password;
+    MaterialButton btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initElement();
+
+        String usernm = "admin";
+        String pass = "admin123";
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals(usernm) && password.getText().toString().equals(pass)) {
+                    Toast.makeText(MainActivity.this, "Login Succes", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    void initElement() {
+        username = (TextView) findViewById(R.id.username);
+        password = (TextView) findViewById(R.id.password);
+        btnLogin = (MaterialButton) findViewById(R.id.btnLogin);
     }
 }
+
